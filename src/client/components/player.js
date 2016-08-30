18582f7c553c4fd3dc0b62ebd0d57dbb7a56570e
icareus <abarbaro@student.42.fr>
2016-08-31 00:10:30 +0200

    teeny video player

---
+++ src/client/components/player.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const videoPlayer = ({url}) => <video src={url} controls>Test player</video>
+
+export default videoPlayer
--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
-}
+import Player from './components/player'
+
+const URL = `http://dl1.kingmovie.co/Movie/2015/Bluray/Mojave.2015.BluRay.720p.x24.KingMovie.Co.mkv`
+
+// class App extends React.Component {
+//   render () {
+//     return <p>Hello React!</p>;
+//   }
+// }
+
+const App = () => (
+  <Player url={`${URL}`} />
+)
 
 render(<App/>, document.getElementById('app'));
